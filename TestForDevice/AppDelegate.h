294ad7262e0ba365ab0b443b6fd9acece7db2874
shangzhixin <shangzhixin@sumaott.com>
2018-03-02 09:10:00 +0800

    Initial Commit

---
+++ TestForDevice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForDevice
+//
+//  Created by dvt04 on 2018/3/2.
+//  Copyright © 2018年 dvt04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForDevice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForDevice
+//
+//  Created by dvt04 on 2018/3/2.
+//  Copyright © 2018年 dvt04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
